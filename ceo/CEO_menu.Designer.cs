ff6941fe3535af40f520f039507dfd1c40556e7c
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-05-09 21:17:43 +0500

    dsgfj

--- ceo/CEO_menu.Designer.cs
+++ ceo/CEO_menu.Designer.cs
@@ -37,27 +37,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 135);
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 60);
+            this.button1.Size = new System.Drawing.Size(150, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "Управление компанией";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 69);
+            this.button2.Location = new System.Drawing.Point(2, 45);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(225, 60);
+            this.button2.Size = new System.Drawing.Size(150, 39);
             this.button2.TabIndex = 1;
             this.button2.Text = "Управление сотрудниками";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Location = new System.Drawing.Point(2, 88);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(225, 60);
+            this.button3.Size = new System.Drawing.Size(150, 39);
             this.button3.TabIndex = 2;
             this.button3.Text = "Работа с поставщиками";
             this.button3.UseVisualStyleBackColor = true;
@@ -65,21 +68,23 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
             this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(457, 140);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(305, 91);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(529, 291);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(353, 189);
             this.flowLayoutPanel1.TabIndex = 3;
             // 
             // CEO_menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.ClientSize = new System.Drawing.Size(785, 484);
             this.Controls.Add(this.flowLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CEO_menu";
             this.Text = "CEO_menu";
             this.flowLayoutPanel1.ResumeLayout(false);

29bc4ac838f7ba8c169632d7e8e068e47e512cf3
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-05-09 21:39:55 +0500

    sussy wussy

--- ceo/CEO_company_management.cs
+++ ceo/CEO_company_management.cs
@@ -16,5 +16,10 @@ namespace NEfotobudka_githubik.ceo
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

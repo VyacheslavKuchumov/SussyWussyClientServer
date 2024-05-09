d925f4244fd0111ff36546fcb9c8ea3b37127f03
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-05-09 21:42:01 +0500

    amogus

--- ceo/CEO_menu.cs
+++ ceo/CEO_menu.cs
@@ -17,6 +17,9 @@ namespace NEfotobudka_githubik.ceo
             InitializeComponent();
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
 
+        }
     }
 }

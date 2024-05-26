9b7066f724911147b348978eb65e4a02276b5e4c
VyacheslavKuchumov <Kuchumov76@mail.ru>
2024-05-26 15:21:15 +0500

    CEO suppliers done

--- ceo/CEO_suppliers.Designer.cs
+++ ceo/CEO_suppliers.Designer.cs
@@ -34,19 +34,19 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxCEOSurname = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxCEOName = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBoxCEOOtchestvo = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -127,19 +127,19 @@
             // 
             this.flowLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.flowLayoutPanel3.Controls.Add(this.label5);
-            this.flowLayoutPanel3.Controls.Add(this.textBox4);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxID);
             this.flowLayoutPanel3.Controls.Add(this.label6);
-            this.flowLayoutPanel3.Controls.Add(this.textBox2);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxName);
             this.flowLayoutPanel3.Controls.Add(this.label7);
-            this.flowLayoutPanel3.Controls.Add(this.textBox1);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxAdress);
             this.flowLayoutPanel3.Controls.Add(this.label2);
-            this.flowLayoutPanel3.Controls.Add(this.textBox5);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxCEOSurname);
             this.flowLayoutPanel3.Controls.Add(this.label3);
-            this.flowLayoutPanel3.Controls.Add(this.textBox6);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxCEOName);
             this.flowLayoutPanel3.Controls.Add(this.label4);
-            this.flowLayoutPanel3.Controls.Add(this.textBox7);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxCEOOtchestvo);
             this.flowLayoutPanel3.Controls.Add(this.label9);
-            this.flowLayoutPanel3.Controls.Add(this.textBox8);
+            this.flowLayoutPanel3.Controls.Add(this.textBoxPhone);
             this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel3.Location = new System.Drawing.Point(63, 3);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
@@ -156,13 +156,14 @@
             this.label5.TabIndex = 15;
             this.label5.Text = "Код ";
             // 
-            // textBox4
+            // textBoxID
             // 
-            this.textBox4.Location = new System.Drawing.Point(4, 25);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(148, 26);
-            this.textBox4.TabIndex = 16;
+            this.textBoxID.Location = new System.Drawing.Point(4, 25);
+            this.textBoxID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(148, 26);
+            this.textBoxID.TabIndex = 16;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label6
             // 
@@ -174,13 +175,13 @@
             this.label6.TabIndex = 18;
             this.label6.Text = "Название";
             // 
-            // textBox2
+            // textBoxName
             // 
-            this.textBox2.Location = new System.Drawing.Point(4, 81);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(148, 26);
-            this.textBox2.TabIndex = 12;
+            this.textBoxName.Location = new System.Drawing.Point(4, 81);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(148, 26);
+            this.textBoxName.TabIndex = 12;
             // 
             // label7
             // 
@@ -192,13 +193,13 @@
             this.label7.TabIndex = 20;
             this.label7.Text = "Адрес поставщика";
             // 
-            // textBox1
+            // textBoxAdress
             // 
-            this.textBox1.Location = new System.Drawing.Point(4, 137);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(148, 26);
-            this.textBox1.TabIndex = 11;
+            this.textBoxAdress.Location = new System.Drawing.Point(4, 137);
+            this.textBoxAdress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(148, 26);
+            this.textBoxAdress.TabIndex = 11;
             // 
             // label2
             // 
@@ -210,13 +211,13 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Фамилия директора";
             // 
-            // textBox5
+            // textBoxCEOSurname
             // 
-            this.textBox5.Location = new System.Drawing.Point(4, 193);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(148, 26);
-            this.textBox5.TabIndex = 17;
+            this.textBoxCEOSurname.Location = new System.Drawing.Point(4, 193);
+            this.textBoxCEOSurname.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxCEOSurname.Name = "textBoxCEOSurname";
+            this.textBoxCEOSurname.Size = new System.Drawing.Size(148, 26);
+            this.textBoxCEOSurname.TabIndex = 17;
             // 
             // label3
             // 
@@ -228,13 +229,13 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Имя директора";
             // 
-            // textBox6
+            // textBoxCEOName
             // 
-            this.textBox6.Location = new System.Drawing.Point(4, 249);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(148, 26);
-            this.textBox6.TabIndex = 19;
+            this.textBoxCEOName.Location = new System.Drawing.Point(4, 249);
+            this.textBoxCEOName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxCEOName.Name = "textBoxCEOName";
+            this.textBoxCEOName.Size = new System.Drawing.Size(148, 26);
+            this.textBoxCEOName.TabIndex = 19;
             // 
             // label4
             // 
@@ -246,13 +247,13 @@
             this.label4.TabIndex = 14;
             this.label4.Text = "Отчество директора";
             // 
-            // textBox7
+            // textBoxCEOOtchestvo
             // 
-            this.textBox7.Location = new System.Drawing.Point(4, 305);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(148, 26);
-            this.textBox7.TabIndex = 21;
+            this.textBoxCEOOtchestvo.Location = new System.Drawing.Point(4, 305);
+            this.textBoxCEOOtchestvo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxCEOOtchestvo.Name = "textBoxCEOOtchestvo";
+            this.textBoxCEOOtchestvo.Size = new System.Drawing.Size(148, 26);
+            this.textBoxCEOOtchestvo.TabIndex = 21;
             // 
             // label9
             // 
@@ -264,13 +265,13 @@
             this.label9.TabIndex = 23;
             this.label9.Text = "Телефон";
             // 
-            // textBox8
+            // textBoxPhone
             // 
-            this.textBox8.Location = new System.Drawing.Point(4, 361);
-            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(148, 26);
-            this.textBox8.TabIndex = 24;
+            this.textBoxPhone.Location = new System.Drawing.Point(4, 361);
+            this.textBoxPhone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(148, 26);
+            this.textBoxPhone.TabIndex = 24;
             // 
             // flowLayoutPanel1
             // 
@@ -294,6 +295,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Поиск по названию";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -303,6 +305,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Заключить договор";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -312,6 +315,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Расторгнуть договор по коду";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button5
             // 
@@ -351,6 +355,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CEO_suppliers";
             this.Text = "CEO_suppliers";
+            this.Load += new System.EventHandler(this.CEO_suppliers_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel2.ResumeLayout(false);
             this.flowLayoutPanel2.PerformLayout();
@@ -371,19 +376,19 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxID;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxAdress;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxCEOSurname;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBoxCEOName;
+        private System.Windows.Forms.TextBox textBoxCEOOtchestvo;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBoxPhone;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- ceo/CEO_suppliers.cs
+++ ceo/CEO_suppliers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,99 @@ namespace NEfotobudka_githubik.ceo
             form.Show();
             // Optionally, hide the current form
             this.Hide();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CEO_suppliers_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-E1GMR9K;Initial Catalog=NE_photo_DB;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Поставщик", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == "")
+            {
+                SqlConnection conn = new SqlConnection("Data Source=DESKTOP-E1GMR9K;Initial Catalog=NE_photo_DB;Integrated Security=True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Поставщик", conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection("Data Source=DESKTOP-E1GMR9K;Initial Catalog=NE_photo_DB;Integrated Security=True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Поставщик WHERE Название_поставщика = @name", conn);
+                cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-E1GMR9K;Initial Catalog=NE_photo_DB;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Поставщик VALUES(@name, @adress, @CEOsurname, @CEOname, @CEOotchestvo, @phone)", conn);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@adress", textBoxAdress.Text);
+            cmd.Parameters.AddWithValue("@CEOsurname", textBoxCEOSurname.Text);
+            cmd.Parameters.AddWithValue("@CEOname", textBoxCEOName.Text);
+            cmd.Parameters.AddWithValue("@CEOotchestvo", textBoxCEOOtchestvo.Text);
+            cmd.Parameters.AddWithValue("@phone", textBoxPhone.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cmd = new SqlCommand("SELECT * FROM Поставщик", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBoxID.Text == "")
+            {
+                MessageBox.Show("Введите код");
+            }
+            else
+            {
+
+                SqlConnection conn = new SqlConnection("Data Source=DESKTOP-E1GMR9K;Initial Catalog=NE_photo_DB;Integrated Security=True");
+                conn.Open();
+
+
+                SqlCommand cmd = new SqlCommand("DELETE FROM Поставщик WHERE Код_поставщика = @id", conn);
+                cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("SELECT * FROM Поставщик", conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+                conn.Close();
+
+            }
         }
     }
 }
